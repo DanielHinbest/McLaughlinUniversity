7c37e7396d3f319f6428cb79502534d9570e799a
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2021-10-19 15:13:44 -0400

    more designs created

---
+++ McLaughlinUniversity/TargetsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace McLaughlinUniversity
+{
+    /// <summary>
+    /// Interaction logic for TargetsWindow.xaml
+    /// </summary>
+    public partial class TargetsWindow : Window
+    {
+        public TargetsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
