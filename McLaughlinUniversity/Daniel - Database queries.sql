c670cd4519d7abf6eced3b25149c5ace9eb1024c
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2021-11-03 15:39:28 -0400

    added Daniel's database queries, fixed some startup issues

---
+++ McLaughlinUniversity/Daniel - Database queries.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE tblDonorTargets (
+	donorTargetID INT UNIQUE NOT NULL,
+	targetID INT NOT NULL,
+	donorID INT NOT NULL,
+	PRIMARY KEY (donorTargetID),
+	FOREIGN KEY (targetID) REFERENCES tblTargets(targetID),
+	FOREIGN KEY (donorID) REFERENCES tblDonors(donorID)
+);
+
+CREATE TABLE tblProgramTargets (
+	programTargetID INT UNIQUE NOT NULL,
+	targetID INT NOT NULL,
+	programID INT NOT NULL,
+	PRIMARY KEY (donorTargetID),
+	FOREIGN KEY (targetID) REFERENCES tblTargets(targetID),
+	FOREIGN KEY (programID) REFERENCES tblPrograms(programID)
+);
+
+CREATE TABLE tblUsers (
+	userID VARCHAR(10) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO tblDonorTargets VALUES (1, 1, 1);
+INSERT INTO tblDonorTargets VALUES (2, 2, 2);
+INSERT INTO tblDonorTargets VALUES (3, 3, 3);
+INSERT INTO tblDonorTargets VALUES (4, 4, 4);
+INSERT INTO tblDonorTargets VALUES (5, 5, 5);
+
+INSERT INTO tblProgramTargets VALUES (1, 1, 1);
+INSERT INTO tblProgramTargets VALUES (2, 2, 2);
+INSERT INTO tblProgramTargets VALUES (3, 3, 3);
+INSERT INTO tblProgramTargets VALUES (4, 4, 4);
+INSERT INTO tblProgramTargets VALUES (5, 5, 5);
+
+INSERT INTO tblUsers VALUES ('Daniel', 
