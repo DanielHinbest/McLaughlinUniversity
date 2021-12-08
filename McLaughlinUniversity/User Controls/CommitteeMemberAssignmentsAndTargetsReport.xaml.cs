2b85bf24c0bf3d241afb457da05829543f1ba2fa
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2021-12-08 18:34:10 -0500

    beta version

--- McLaughlinUniversity/User Controls/CommitteeMemberAssignmentsAndTargetsReport.xaml.cs
+++ McLaughlinUniversity/User Controls/CommitteeMemberAssignmentsAndTargetsReport.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace McLaughlinUniversity
 {
@@ -21,6 +23,53 @@ namespace McLaughlinUniversity
         public CommitteeMemberAssignmentsAndTargetsReport()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopulateGrid()
+        {
+            //Try/Catch exception handling
+            try
+            {
+                int year = Convert.ToInt32(cmbYear.Text);
+                //Stores the connection settings in the variable
+                string connectString = DataAccess.GetConnectionString();
+
+                //Connection for the SQL database
+                SqlConnection connection = new SqlConnection(connectString);
+
+                //Opens the connection
+                connection.Open();
+
+                //SQL search query
+                string selectRecords = "";
+                //Executes the command
+                SqlCommand command = new SqlCommand(selectRecords, connection);
+
+                //Retrieves the data from the database
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+                //A new data table for the targets in the database
+                DataTable data = new DataTable("Targets");
+
+                //Fills the data adapter with the information from the data table
+                dataAdapter.Fill(data);
+
+                //Outputs the items to the screen
+                dgCommitteememberAssignmentsAndTargets.ItemsSource = data.DefaultView;
+
+                //Closes the connection
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                //Outputs the error to the user
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
