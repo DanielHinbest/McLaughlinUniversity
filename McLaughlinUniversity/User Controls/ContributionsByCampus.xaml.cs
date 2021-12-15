56d5878be7033c6c4f621a5d5b0e2cd110b8b798
xClayson <60987447+xClayson@users.noreply.github.com>
2021-12-14 22:07:14 -0500

    Add files via upload

--- McLaughlinUniversity/User Controls/ContributionsByCampus.xaml.cs
+++ McLaughlinUniversity/User Controls/ContributionsByCampus.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace McLaughlinUniversity
 {
@@ -22,5 +24,59 @@ namespace McLaughlinUniversity
         {
             InitializeComponent();
         }
+
+        private void PopulateGrid()
+        {
+            //Try/Catch exception handling
+            try
+            {
+                int year = Convert.ToInt32(cmbYear.Text);
+                //Stores the connection settings in the variable
+                string connectString = DataAccess.GetConnectionString();
+
+                //Connection for the SQL database
+                SqlConnection connection = new SqlConnection(connectString);
+
+                //Opens the connection
+                connection.Open();
+
+                //SQL search query
+               // string selectRecords = "SELECT tblCampus.campusName as 'Campus Name', SUM(tblTargets.firstQuarterTarget) as 'This Quarter', (SUM(transactionAmount) / firstQuarterTarget) *100 as '% of target achieved' " +
+               //     "FROM tblCampus, ((tblProgramTargets " +
+               //     "INNER JOIN tblTargets ON tblProgramTargets.targetID = tblTargets.targetID) " +
+               //     "INNER JOIN tblPrograms ON tblPrograms.campusName = campusName) " +
+               //     "WHERE yearNo = " + year +
+               //     "GROUP BY tblCampus.campusName;";
+
+                //Executes the command
+                SqlCommand command = new SqlCommand(selectRecords, connection);
+
+                //Retrieves the data from the database
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+                //A new data table for the targets in the database
+                DataTable data = new DataTable("Targets");
+
+                //Fills the data adapter with the information from the data table
+                dataAdapter.Fill(data);
+
+                //Outputs the items to the screen
+                dgContributionsByCampusReport.ItemsSource = data.DefaultView;
+
+                //Closes the connection
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                //Outputs the error to the user
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            PopulateGrid();
+        }
     }
 }
+
